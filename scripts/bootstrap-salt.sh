1ad7729d430841462db314de2c6d5d89889e7cbc
Stuart Munro <stuart@munro.co>
2015-04-16 13:59:16 +0100

    Change bootstrap-salt.sh to not install everytime it's run

--- scripts/bootstrap-salt.sh
+++ scripts/bootstrap-salt.sh
@@ -1,19 +1,23 @@
 #!/bin/bash -xe
 
 # SETUP EC2 INSTANCE
-apt-get update
-apt-get -y install python-setuptools git
-easy_install boto
-
-# get or update bootstrap-salt
-if [ -d "/usr/local/bootstrap-salt" ]; then
-  cd /usr/local/bootstrap-salt && git pull
+T1=`dpkg -l | grep python-setuptools | wc -l`
+T2="1"
+if [ "$T1" = "$T2" ]; then
+  echo "[INFO] Base packages already installed..."
 else
-  cd /usr/local && git clone https://github.com/ministryofjustice/bootstrap-salt
+  apt-get update
+  apt-get -y install python-setuptools git
+  easy_install boto
 fi
 
-easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
-chmod 755 /usr/local/bootstrap-salt/scripts/ec2_tags.py
-chmod 750 /usr/local/bootstrap-salt/bootstrap_salt/salt_utils.py
-/usr/local/bootstrap-salt/scripts/ec2_tags.py
-
+# get bootstrap-salt, doesn't do upgrade anymore
+if [ -d "/usr/local/bootstrap-salt" ]; then
+  echo "[INFO] boostrap-salt already installed..."
+else
+  cd /usr/local && git clone https://github.com/ministryofjustice/bootstrap-salt
+  easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
+  chmod 755 /usr/local/bootstrap-salt/scripts/ec2_tags.py
+  chmod 750 /usr/local/bootstrap-salt/bootstrap_salt/salt_utils.py
+  /usr/local/bootstrap-salt/scripts/ec2_tags.py
+fi
