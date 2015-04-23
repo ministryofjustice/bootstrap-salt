c2cf3cb95a5cb9b57e4b14456e2601d9ddec6f2a
mattmb <matthew.mead-briggs@digital.justice.gov.uk>
2015-04-23 15:04:22 +0100

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bootstrap_salt',
-    version='0.0.3',
+    version='0.0.4',
     url='http://github.com/ministryofjustice/bootstrap-salt/',
     license='LICENSE',
     author='MOJDS',
