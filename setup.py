9b87856f282fc6f6b74fc4d7c28b02df915675d4
Ciprian Ciubotariu <ciprianc@gmail.com>
2015-05-26 12:16:10 +0100

    New version: 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bootstrap_salt',
-    version='0.1.2',
+    version='0.2.0',
     url='http://github.com/ministryofjustice/bootstrap-salt/',
     license='LICENSE',
     author='MOJDS',
