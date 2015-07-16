8804e9a3ebdfee10c4ac7fec0a21089acea81b09
mattmb <matthew.mead-briggs@digital.justice.gov.uk>
2015-07-16 17:38:47 +0100

    Create new bootstrap framework
    Create a contrib dir with pre-reqs for a highstate and a bootstrap
    script. Refactor utility script to call salt locally rather than via
    master. WIP on adding utility script function to pull salt content.

--- tests/test_salt_util.py
+++ tests/test_salt_util.py
@@ -16,93 +16,36 @@ class SaltUtilTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_get_minions_batch(self):
-        mock_result = mock.Mock()
-        # No batch
-        mock_config = {'cmd.return_value': {'minion1': 'blah',
-                                            'minion2': 'blah'}}
-        mock_result.configure_mock(**mock_config)
-        mock_client = mock.Mock()
-        mock_client.return_value = mock_result
-        mock_c = mock.Mock(LocalClient=mock_client)
-        salt.client = mock_c
-        x = salt_utils.get_minions_batch('*')
-        expected = [['minion1', 'minion2']]
-        self.assertEqual(x, expected)
-
-        # 50% batch
-        mock_config = {'cmd.return_value': {'minion1': 'blah',
-                                            'minion2': 'blah',
-                                            'minion3': 'blah',
-                                            'minion4': 'blah'}}
-        mock_result.configure_mock(**mock_config)
-        mock_client = mock.Mock()
-        mock_client.return_value = mock_result
-        mock_c = mock.Mock(LocalClient=mock_client)
-        salt.client = mock_c
-
-        x = salt_utils.get_minions_batch('*', 0.5)
-        expected = [['minion4', 'minion1'], ['minion3', 'minion2']]
-        self.assertEqual(x, expected)
-
-        # 50% batch uneven minons
-        mock_config = {'cmd.return_value': {'minion1': 'blah',
-                                            'minion2': 'blah',
-                                            'minion3': 'blah'}}
-        mock_result.configure_mock(**mock_config)
-        mock_client = mock.Mock()
-        mock_client.return_value = mock_result
-        mock_c = mock.Mock(LocalClient=mock_client)
-        salt.client = mock_c
-
-        x = salt_utils.get_minions_batch('*', 0.5)
-        expected = [['minion1', 'minion3'], ['minion2']]
-        self.assertEqual(x, expected)
-
     def test_state_result(self):
         salt.config = mock.Mock()
         mock_result = mock.Mock()
-        mock_config = {'cmd.return_value': {'minon1': {'state': {'result': True}}}}
+        mock_config = {'function.return_value': {'state': {'result': True}}}
         mock_result.configure_mock(**mock_config)
 
         mock_client = mock.Mock()
         mock_client.return_value = mock_result
 
-        mock_runner = mock.Mock(RunnerClient=mock_client)
+        mock_caller = mock.Mock(Caller=mock_client)
 
-        salt.runner = mock_runner
-        x = salt_utils.state_result('12345')
+        salt.client = mock_caller
+        salt_utils.get_salt_data = mock.Mock()
+        x = salt_utils.state('12345')
         self.assertTrue(x)
 
-    def test_no_state_result(self):
-        salt.config = mock.Mock()
-        mock_result = mock.Mock()
-        mock_config = {'cmd.return_value': {}}
-        mock_result.configure_mock(**mock_config)
-
-        mock_client = mock.Mock()
-        mock_client.return_value = mock_result
-
-        mock_runner = mock.Mock(RunnerClient=mock_client)
-
-        salt.runner = mock_runner
-        x = salt_utils.state_result('12345')
-        self.assertFalse(x)
-
     def test_check_state_result_good(self):
-        result = {'minon1': {'state': {'result': True}},
-                  'minion2': {'state': {'result': True}}}
+        result = {'state': {'result': True},
+                  'state1': {'result': True}}
         x = salt_utils.check_state_result(result)
         self.assertTrue(x)
 
     def test_check_state_result_bad(self):
-        result = {'minon1': {'state': {'result': False}},
-                  'minion2': {'state': {'result': True}}}
+        result = {'state': {'result': False},
+                  'state1': {'result': True}}
         with self.assertRaises(salt_utils.SaltStateError):
             salt_utils.check_state_result(result)
 
     def test_check_state_result_parse_error(self):
-        result = {'minon1': ['SOME SALT PARSER ERROR']}
+        result = ['SOME SALT PARSER ERROR']
         with self.assertRaises(salt_utils.SaltParserError):
             salt_utils.check_state_result(result)
 
