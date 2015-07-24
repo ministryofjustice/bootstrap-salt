b60d425f1e3a937ec5ff42db0b1f3b2ee7bb3ca2
mattmb <matthew.mead-briggs@digital.justice.gov.uk>
2015-07-24 13:05:16 +0100

    Add custom config parser
    This adds a custom config parser to extend the config in bootstrap-cfn.
    It allows us to add a bootsrap script and some KMS data to the launch
    config.

---
+++ bootstrap_salt/config.py
@@ -0,0 +1,37 @@
+import os
+import pkgutil
+
+from bootstrap_cfn.config import ConfigParser
+
+from fabric.api import env
+
+import yaml
+
+
+class MyConfigParser(ConfigParser):
+
+    def base_template(self):
+        ret = super(MyConfigParser, self).base_template()
+        ret.add_mapping('KMS', {'salt': {'key': env.kms_key_id}})
+        return ret
+
+    def get_ec2_userdata(self):
+        ret = super(MyConfigParser, self).get_ec2_userdata()
+
+        bs_path = os.path.dirname(pkgutil.get_loader('bootstrap_salt').filename)
+        script = os.path.join(bs_path, './contrib/bootstrap.sh')
+        files = {'write_files': [{'encoding': 'b64',
+                                  'content': env.kms_data_key,
+                                  'owner': 'root:root',
+                                  'path': '/etc/salt.key.enc',
+                                  'permissions': '0600'},
+                                 {'content': open(script).read(),
+                                  'owner': 'root:root',
+                                  'path': '/tmp/bootstrap.sh',
+                                  'permissions': '0700'}]}
+
+        ret.append({
+            'content': yaml.dump(files),
+            'mime_type': 'text/cloud-config'
+        })
+        return ret
---
+++ tests/test_config.py
@@ -0,0 +1,40 @@
+import unittest
+
+import bootstrap_salt.config
+from bootstrap_salt.config import MyConfigParser
+
+from mock import patch
+
+from testfixtures import compare
+from troposphere import Template
+
+
+class TestConfig(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    # http://mock.readthedocs.org/en/latest/patch.html#where-to-patch
+    @patch('bootstrap_salt.config.ConfigParser.base_template')
+    @patch.object(bootstrap_salt.config, 'env')
+    def test_base_template(self, mock_env, mock_super):
+        mock_super.return_value = Template()
+        mock_env.kms_key_id = 'fake-key-id'
+        x = MyConfigParser({}, 'my-stack').base_template()
+        compare(x.mappings['KMS'], {'salt': {'key': 'fake-key-id'}})
+
+    # http://mock.readthedocs.org/en/latest/patch.html#where-to-patch
+    @patch('bootstrap_salt.config.ConfigParser.get_ec2_userdata')
+    @patch.object(bootstrap_salt.config, 'env')
+    def test_get_ec2_userdata(self, mock_env, mock_super):
+        mock_super.return_value = []
+        mock_env.kms_data_key = 'fake-key-data'
+
+        expected = [{'content': 'runcmd: [/tmp/bootstrap.sh vmmb]\n', 'mime_type': 'text/cloud-config'},
+                    {'content': "write_files:\n- {content: fake-key-data, encoding: b64, owner: 'root:root', path: /etc/salt.key.enc,\n  permissions: '0600'}\n- {content: '#!/bin/bash\n\n    REVISION=$1\n\n    wget https://raw.githubusercontent.com/saltstack/salt-bootstrap/6080a18e6c7c2d49335978fa69fa63645b45bc2a/bootstrap-salt.sh\n    -O /tmp/bootstrap-salt.sh\n\n    chmod 700 /tmp/bootstrap-salt.sh\n\n    apt-get update && apt-get -y install python-pip git python-dev\n\n    pip install boto\n\n    pip install gnupg\n\n    cd /tmp\n\n    git clone --depth 1 --branch $REVISION https://github.com/ministryofjustice/bootstrap-salt.git\n\n    cd ./bootstrap-salt\n\n    cp -Lrf ./contrib/* /\n\n    /tmp/bootstrap-salt.sh git v2014.7.5\n\n    salt-call saltutil.sync_all\n\n    /usr/local/bin/salt_utils.py -s highstate\n\n    touch /tmp/bootstrap_done\n\n    ', owner: 'root:root', path: /tmp/bootstrap.sh, permissions: '0700'}\n", 'mime_type': 'text/cloud-config'}]  # NOQA
+
+        x = MyConfigParser({}, 'my-stack').get_ec2_userdata()
+        compare(x, expected)
+
+    def tearDown(self):
+        pass
