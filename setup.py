a5aa1f212bc374bc52ebbf4812ac4d2a20f85c51
mattmb <matthew.mead-briggs@digital.justice.gov.uk>
2015-08-03 14:26:05 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bootstrap_salt',
-    version='1.0.0',
+    version='1.0.1',
     url='http://github.com/ministryofjustice/bootstrap-salt/',
     license='LICENSE',
     author='MOJDS',
