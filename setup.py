6eae0e409bf8f137b550a75b08813fc5421f1e9e
Ash Berlin <ash_github@firemirror.com>
2015-08-10 13:03:53 +0100

    Bump version in setup.py for release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bootstrap_salt',
-    version='1.0.2',
+    version='1.1.0',
     url='http://github.com/ministryofjustice/bootstrap-salt/',
     license='LICENSE',
     author='MOJDS',
