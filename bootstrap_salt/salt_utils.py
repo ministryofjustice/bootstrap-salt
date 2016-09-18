c7a89b8213bca387a837b4ae8b9a5b663d847aa6
Niall Creech <niall.creech@digital.justice.gov.uk>
2016-09-18 07:27:47 +0100

    Refactor salt_utils to synchronise remote data more reliably
    The current 2014.7 version of SaltStack doesn't handle updating
    or synchronising dynamic module data and then accessing it in a
    consistent manner. This leads to to inconsistencies in the versions
    of custom grains, states, etc, available at different points throughout
    a set of salt operations loaded into memory. Note that later versions
    of salt deal with this situation better with the addition of
    reload_grains and similar new methods to update internal state to reflect
    the updated dynamic data during the lifetime of a salt-minion.
    
    When the salt python modules are loaded in the script, any sequential
    or concurrent operations can get custom grain information in a range of
    states from fully updated to all custom grain data missing. This is due
    to the data being accessed in a range of different ways dependent on the
    operation, from accessing the salt.loader which might hold fully updated
    data, to accessing through __grains__ such as in the template parsing
    and rendering of pillar data, which might not reflect any changes
    since the modules were loaded. This is an understood consequence
    of the way and order salt updates grains and modules, with some
    mitigation in later salt versions.
    
    Since we don't use master/minions setups, and hence are not running
    salt-minion. This means that we do not have a problem with salts
    expectation that grains are relatively static throughout the lifetime
    of a minion, and only that they are static throughout the
    lifetime of a salt call instantiation.
    
    This change takes a simple approach to consistently downloading and
    synchronising data while maintain back compatibility. The original
    salt_utils script is seperated out into two distinct scripts, one to
    download and synchronise salt config data, the second to apply the state.
    Along with some additional caching and syncing to provide some robustness
    in the active data state, his ensures that the data accessed by the salt
    state called reflects that that is expected.
    
    * Move update and state application functionality into distinct scripts
    * Seperate out data synchronising code into a distinct functions and scripts
    * Improve the quality of logging
    * Allow specification of logging level on the command line
    * Replace external subprocess calls to tar with in-code tarfile calls
    * Fix pep8 warnings
    * Handle syntax parsing of saltstack output to print errors better

--- bootstrap_salt/salt_utils.py
+++ bootstrap_salt/salt_utils.py
@@ -1,123 +1,31 @@
 #!/usr/bin/env python
-import base64
-import os
+import argparse
 import subprocess
-import sys
-
-import salt
-import salt.client
-import salt.config
-import salt.output
-import salt.runner
-
-
-class BootstrapUtilError(Exception):
-
-    def __init__(self, msg):
-        print >> sys.stderr, "[ERROR] {0}: {1}".format(self.__class__.__name__, msg)
-
-
-class SaltStateError(BootstrapUtilError):
-    pass
-
-
-class SaltParserError(BootstrapUtilError):
-    pass
-
-
-def get_salt_data():
-    import boto.s3
-    import boto.kms
-    import gnupg
-    import shutil
-
-    caller = salt.client.Caller()
-    stack_name = caller.function('grains.item', 'aws:cloudformation:stack-name')['aws:cloudformation:stack-name']
-    region = caller.function('grains.item', 'aws_region')['aws_region']
-    kms = boto.kms.connect_to_region(region)
-    s3 = boto.s3.connect_to_region(region)
-    tar_file = s3.get_bucket('{0}-salt'.format(stack_name)).get_key('srv.tar.gpg')
-    if tar_file:
-        tar_file.get_contents_to_filename('/srv.tar.gpg')
-        os.chmod('/srv.tar.gpg', 0700)
-        key = kms.decrypt(open('/etc/salt.key.enc').read())['Plaintext']
-        key = base64.b64encode(key)
-        gpg = gnupg.GPG()
-        gpg.decrypt_file(open('/srv.tar.gpg'), passphrase=key,
-                         output='/srv.tar')
-        os.chmod('/srv.tar', 0700)
-    if not tar_file and not os.path.isfile('/srv.tar'):
-        print "Salt tar not found, probably this is an initial bootstrap"
-        sys.exit(0)
-    shutil.rmtree('/srv/salt', ignore_errors=True)
-    shutil.rmtree('/srv/pillar', ignore_errors=True)
-    subprocess.call(['tar', '--no-same-owner', '-xvf', '/srv.tar', '-C', '/'])
-
-
-def highstate():
-    '''
-    Raises:
-        SaltParserError: if any minion cannot execute the state
-        SaltStateError: if any state execution returns False
-    '''
-    get_salt_data()
-    caller = salt.client.Caller()
-    # synchronizes custom modules, states, beacons, grains, returners,
-    # output modules, renderers, and utils.
-    caller.function('saltutil.sync_all')
-    res = caller.function('state.highstate')
-    return check_state_result(res)
-
-
-def state(state):
-    '''
-    Raises:
-        SaltParserError: if any minion cannot execute the state
-        SaltStateError: if any state execution returns False
-    Args:
-        state(string): the state to run
-    '''
-    get_salt_data()
-    caller = salt.client.Caller()
-    res = caller.function('state.sls', state)
-    return check_state_result(res)
-
-
-def check_state_result(result):
-    '''
-    Takes a salt results dictionary, prints the ouptut in salts highstate ouput
-    format and checks all states executed successfully. Returns True or raises:
-    Raises:
-        SaltParserError: if any minion cannot execute the state
-        SaltStateError: if any state execution returns False
-    Args:
-        result(dict): salt results dictionary
-    '''
-    __opts__ = salt.config.minion_config('/etc/salt/minion')
-    salt.output.display_output({'local': result}, out='highstate', opts=__opts__)
-    if isinstance(result, dict):
-        results = [v['result'] for v in result.values()]
-    else:
-        raise SaltParserError('Minion could not parse state data')
-    if all(results):
-        return True
-    else:
-        raise SaltStateError('State did not execute successfully')
 
 if __name__ == "__main__":
-    import argparse
-
-    from salt.log.setup import setup_console_logger, setup_logfile_logger
-
-    parser = argparse.ArgumentParser(description='Run salt states')
-    parser.add_argument('-s', dest='state', type=str,
-                        help='Name of state or "highstate"', required=True)
-
-    setup_console_logger(log_level='info')
-    setup_logfile_logger(log_path='/var/log/salt/minion', log_level='debug')
-
+    parser = argparse.ArgumentParser(
+        description=('Update the salt config '
+                     'from a remote s3 store and run a salt state')
+    )
+    parser.add_argument('-s',
+                        dest='state',
+                        type=str,
+                        help='Name of state or highstate',
+                        required=True)
+    parser.add_argument('--loglevel',
+                        dest='loglevel',
+                        type=str,
+                        help=('Level of logging detail, '
+                              'debug, info, warning, error or critical'),
+                        default="info")
     args = parser.parse_args()
-    if args.state == "highstate":
-        highstate()
-    else:
-        state(args.state)
+    # Sync the salt data from an the s3 store and synchronise
+    subprocess.call(["salt_utils_update.py",
+                     "--loglevel",
+                     args.loglevel])
+    # Run the state
+    subprocess.call(["salt_utils_state.py",
+                     "-s",
+                     args.state,
+                     "--loglevel",
+                     args.loglevel])
