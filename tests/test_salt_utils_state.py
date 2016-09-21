0618d8b557cf2912b96f9f5a92afe50e497e50b7
Niall Creech <niall.creech@digital.justice.gov.uk>
2016-09-21 08:40:59 +0100

    Update salt_utils_state tests to use proper mocking

--- tests/test_salt_utils_state.py
+++ tests/test_salt_utils_state.py
@@ -1,58 +1,175 @@
 import unittest
-import mock
-import sys
-
-# This is a hack so that we don't need salt to run our tests
-sys.modules['salt'] = mock.Mock()
-sys.modules['salt.runner'] = mock.Mock()
-sys.modules['salt.config'] = mock.Mock()
-sys.modules['salt.output'] = mock.Mock()
-sys.modules['salt.client'] = mock.Mock()
-sys.modules['salt.utils'] = mock.Mock()
-sys.modules['salt.log'] = mock.Mock()
-sys.modules['salt.log.setup'] = mock.Mock()
-import salt
-
+from mock import MagicMock, patch
+from nose.tools import raises
 from bootstrap_salt.salt_utils_state import SaltUtilsStateWrapper, SaltParserError, SaltStateError
 
 
 class SaltUtilsStateTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.salt_utils_state = SaltUtilsStateWrapper()
         pass
 
-    def test_state_result(self):
-        salt.config = mock.Mock()
-        mock_result = mock.Mock()
-        mock_config = {'function.return_value': {'state': {'result': True}}}
-        mock_result.configure_mock(**mock_config)
+    @patch('salt.client.Caller')
+    @patch('salt.config')
+    @patch('salt.output')
+    def test_state_result_good(self,
+                               mock_salt_output,
+                               mock_salt_config,
+                               mock_salt_client_caller):
+        """
+        test_state_result_good: Test calling a state and getting back a correct result
+        """
+        # Construct nested caller function
+        instance = mock_salt_client_caller.return_value
+        instance.function.return_value = MagicMock()
+        instance.function.return_value.return_value = ['This call succeeded']
 
-        mock_client = mock.Mock()
-        mock_client.return_value = mock_result
+        # Make actual call
+        salt_utils_state = SaltUtilsStateWrapper()
+        actual_result = salt_utils_state.state('12345')
 
-        mock_caller = mock.Mock(Caller=mock_client)
+        self.assertTrue(actual_result)
 
-        salt.client = mock_caller
-        x = self.salt_utils_state.state('12345')
-        self.assertTrue(x)
+    @raises(SaltStateError)
+    @patch('salt.client.Caller')
+    @patch('salt.config')
+    @patch('salt.output')
+    def test_state_result_bad(self,
+                              mock_salt_output,
+                              mock_salt_config,
+                              mock_salt_client_caller):
+        """
+        test_state_result_bad: Test calling a state and getting back a failed result
+        """
+        # Construct nested caller function
+        instance = mock_salt_client_caller.return_value
+        instance.function.return_value = MagicMock()
+        instance.function.return_value.return_value = ['This call failed']
 
-    def test_check_state_result_good(self):
+        # Make actual call
+        salt_utils_state = SaltUtilsStateWrapper()
+        salt_utils_state.state('12345')
+
+    @patch('salt.client.Caller')
+    @patch('salt.config')
+    @patch('salt.output')
+    def test_state_result_with_dictionary_good(self,
+                                               mock_salt_output,
+                                               mock_salt_config,
+                                               mock_salt_client_caller):
+        """
+        test_state_result_with_dictionary_good: Test calling a state and getting back a correct result as a dictionary
+        """
+        # Construct nested caller function
+        instance = mock_salt_client_caller.return_value
+        instance.function.return_value = MagicMock()
+        instance.function.return_value.return_value = {'state': {'result': True}}
+
+        # Make actual call
+        salt_utils_state = SaltUtilsStateWrapper()
+        actual_result = salt_utils_state.state('12345')
+        self.assertTrue(actual_result)
+
+    @raises(SaltStateError)
+    @patch('salt.client.Caller')
+    @patch('salt.config')
+    @patch('salt.output')
+    def test_state_result_with_dictionary_bad(self,
+                                              mock_salt_output,
+                                              mock_salt_config,
+                                              mock_salt_client_caller):
+        """
+        test_state_result_with_dictionary_bad: Test calling a state and getting back a failed result as a dictionary
+        """
+        # Construct nested caller function
+        # Construct nested caller function
+        instance = mock_salt_client_caller.return_value
+        instance.function.return_value = MagicMock()
+        instance.function.return_value.return_value = {'state': {'result': False}}
+
+        # Make actual call
+        salt_utils_state = SaltUtilsStateWrapper()
+        salt_utils_state.state('12345')
+
+    @patch('salt.client.Caller')
+    @patch('salt.config')
+    @patch('salt.output')
+    def test_highstate_result_good(self,
+                                   mock_salt_output,
+                                   mock_salt_config,
+                                   mock_salt_client_caller):
+        """
+        test_highstate_result_good: Test calling a highstate and getting back a correct result
+        """
+        # Construct nested caller function
+        instance = mock_salt_client_caller.return_value
+        instance.function.return_value = ['This call succeeded']
+        # Make actual call
+        salt_utils_state = SaltUtilsStateWrapper()
+        actual_result = salt_utils_state.state('highstate')
+        self.assertTrue(actual_result)
+
+    @patch('salt.client.Caller')
+    @patch('salt.config')
+    @patch('salt.output')
+    def test_check_state_result_good(self,
+                                     mock_salt_output,
+                                     mock_salt_config,
+                                     mock_salt_client_caller):
+        """
+        test_check_state_result_good: Test checking good state result
+        """
         result = {'state': {'result': True},
                   'state1': {'result': True}}
-        x = self.salt_utils_state.check_state_result(result)
-        self.assertTrue(x)
+        salt_utils_state = SaltUtilsStateWrapper()
+        result = salt_utils_state.check_state_result(result)
+        self.assertTrue(result)
 
-    def test_check_state_result_bad(self):
+    @raises(SaltStateError)
+    @patch('salt.client.Caller')
+    @patch('salt.config')
+    @patch('salt.output')
+    def test_check_state_result_dictionary_bad(self,
+                                               mock_salt_output,
+                                               mock_salt_config,
+                                               mock_salt_client_caller):
+        """
+        test_check_state_result_dictionary_bad: Test checking failed state result as dictionary
+        """
         result = {'state': {'result': False},
                   'state1': {'result': True}}
-        with self.assertRaises(SaltStateError):
-            self.salt_utils_state.check_state_result(result)
+        salt_utils_state = SaltUtilsStateWrapper()
+        salt_utils_state.check_state_result(result)
 
-    def test_check_state_result_parse_error(self):
-        result = ['SOME SALT PARSER ERROR']
-        with self.assertRaises(SaltParserError):
-            self.salt_utils_state.check_state_result(result)
+    @raises(SaltStateError)
+    @patch('salt.client.Caller')
+    @patch('salt.config')
+    @patch('salt.output')
+    def test_check_state_result_list_bad(self,
+                                         mock_salt_output,
+                                         mock_salt_config,
+                                         mock_salt_client_caller):
+        """
+        test_check_state_result_list_bad: Test bad result as list
+        """
+        result = ['SOME SALT ERROR']
+        salt_utils_state = SaltUtilsStateWrapper()
+        salt_utils_state.check_state_result(result)
+
+    @raises(SaltParserError)
+    @patch('salt.client.Caller')
+    @patch('salt.config')
+    @patch('salt.output')
+    def test_check_state_result_parse_error(self,
+                                            mock_salt_output,
+                                            mock_salt_config,
+                                            mock_salt_client_caller):
+        """
+        test_check_state_result_parse_error: Test badly formatted result
+        """
+        result = 'SOME UNEXPECTED OUTPUT FORMAT'
+        salt_utils_state = SaltUtilsStateWrapper()
+        salt_utils_state.check_state_result(result)
 
     def tearDown(self):
         pass
