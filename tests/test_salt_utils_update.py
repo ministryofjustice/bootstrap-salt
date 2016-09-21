a41f18b56b65cd21b29772e96001d539922d7fda
Niall Creech <niall.creech@digital.justice.gov.uk>
2016-09-21 09:23:57 +0100

    Update salt_utils_update tests

--- tests/test_salt_utils_update.py
+++ tests/test_salt_utils_update.py
@@ -1,42 +1,73 @@
 import unittest
-import mock
-import sys
-
-# This is a hack so that we don't need salt to run our tests
-sys.modules['salt'] = mock.Mock()
-sys.modules['salt.runner'] = mock.Mock()
-sys.modules['salt.config'] = mock.Mock()
-sys.modules['salt.output'] = mock.Mock()
-sys.modules['salt.client'] = mock.Mock()
-sys.modules['salt.utils'] = mock.Mock()
-sys.modules['salt.log'] = mock.Mock()
-sys.modules['salt.log.setup'] = mock.Mock()
-
-import salt
+from mock import call, MagicMock, patch
 from bootstrap_salt.salt_utils_update import SaltUtilsUpdateWrapper
 
 
-class SaltUtilUpdateTestCase(unittest.TestCase):
+class SaltUtilsUpdateTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.salt_utils_update = SaltUtilsUpdateWrapper()
         pass
 
-    def test_state_result(self):
-        salt.config = mock.Mock()
-        mock_result = mock.Mock()
-        mock_config = {'function.return_value': {'state': {'result': True}}}
-        mock_result.configure_mock(**mock_config)
+    @patch('salt.client.Caller')
+    @patch('bootstrap_salt.salt_utils_update.SaltUtilsUpdateWrapper.get_salt_data')
+    def test_sync_remote_salt_data(self,
+                                   mock_get_salt_data,
+                                   mock_salt_client_caller):
+        """
+        test_sync_remote_salt_data: syncing data call makes the expected calls
+        """
+        # Construct nested caller function
+        instance = mock_salt_client_caller.return_value
+        instance.function.return_value = MagicMock()
+        instance.function.return_value.side_effect = [
+            'cache_clear dict',
+            'sync data dict'
+        ]
+        # Make actual call
+        salt_utils_update = SaltUtilsUpdateWrapper()
+        salt_utils_update.sync_remote_salt_data()
+        mock_get_salt_data.assert_called_once()
 
-        mock_client = mock.Mock()
-        mock_client.return_value = mock_result
+        expected_method_calls = [
+            call.function('saltutil.clear_cache'),
+            call.function('saltutil.sync_all', 'refresh=True')
+        ]
+        self.assertEqual(instance.method_calls,
+                         expected_method_calls,
+                         "test_sync_remote_salt_data: salt caller function "
+                         "was not called with all the expected methods")
 
-        mock_caller = mock.Mock(Caller=mock_client)
+    @patch('salt.client.Caller')
+    @patch('bootstrap_salt.salt_utils_update.SaltUtilsUpdateWrapper.get_salt_data')
+    def test_sync_remote_salt_data_no_clear_cache(self,
+                                                  mock_get_salt_data,
+                                                  mock_salt_client_caller):
+        """
+        test_sync_remote_salt_data_no_clear_cache: syncing data call makes the expected calls without clearing cache
+        """
+        # Construct nested caller function
+        instance = mock_salt_client_caller.return_value
+        instance.function.return_value = MagicMock()
+        instance.function.return_value.side_effect = [
+            'cache_clear dict',
+            'sync data dict'
+        ]
+        # Make actual call
+        salt_utils_update = SaltUtilsUpdateWrapper()
+        salt_utils_update.sync_remote_salt_data(clear_cache=False)
+        mock_get_salt_data.assert_called_once()
 
-        salt.client = mock_caller
-        self.salt_utils_update.get_salt_data = mock.Mock()
-        x = False
-        self.assertTrue(x)
+        expected_method_calls = [
+            call.function('saltutil.sync_all', 'refresh=True')
+        ]
+        self.assertEqual(instance.method_calls,
+                         expected_method_calls,
+                         "test_sync_remote_salt_data: salt caller function "
+                         "was not called with all the expected methods. "
+                         "\nactual: {} \nexpected: {}"
+                         .format(instance.method_calls,
+                                 expected_method_calls)
+                         )
 
     def tearDown(self):
         pass
