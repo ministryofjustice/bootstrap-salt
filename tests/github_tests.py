ministryofjustice/bootstrap-salt

commit cbca00eed2235d12903fa3c37170fa4624395298

deprecate_paging_arg

bootstrap_salt/deploy_lib/github.py
@@ -47,38 +47,56 @@ def get_github_token():
     return os.environ['GH_TOKEN']
 
 
-def get_paginated_content(url, page=None, out=None, **kwargs):
+def get_paginated_content(url,
+                          page=None,
+                          out={},
+                          **kwargs):
     """
     Helper function to get information from the github API,
     and handle any paging.
 
     Args:
         url(string): The API url endpoint to contact
-        page():
-        out():
+        page(int): DEPRECATED: The page number of the
+            paginated requests to retrieve.
+        out(dict): Output dictionary to append results to.
         kwargs(dictionary): Other API call parameters
     """
+    per_page = 100
+
+    # Ensure we have a params entry in kwargs
+    if 'params' not in kwargs:
+        kwargs['param'] = {}
+
+    # if we have paging, update the url
+    if page is not None:
+        logger.warning("get_paginated_content: "
+                     "page value is deprecated, ignoring "
+                     " specified value '{}'..."
+                     .format(page))
     if 'auth' not in kwargs:
         kwargs['auth'] = (get_github_token(), 'x-oauth-basic')
 
-    if 'params' in kwargs:
-        kwargs['params'].update({'per_page': 100})
-    else:
-        kwargs['params'] = {'per_page': 100}
-    r = requests.get(url, **kwargs)
-    if r.status_code != 200:
+    kwargs['params'].update({'per_page': per_page})
+
+    # Do requests
+    result = requests.get(url, **kwargs)
+    if result.status_code != 200:
         raise GithubRequestException('GH API request failed with code: {}'
-                                     '\n{}'.format(r.status_code, r.text))
+                                     '\n{}'
+                                     .format(result.status_code,
+                                            result.text)
+                                     )
 
-    if not out:
-        out = r.json()
-    else:
-        out.extend(r.json())
+    out.extend(r.json())
 
-    if 'next' in r.links:
-        return get_paginated_content(r.links['next']['url'], out=out, **kwargs)
-    else:
-        return out
+    # Recursively append paged results to out dictionary
+    if 'next' in result.links:
+        out = get_paginated_content(
+                result.links['next']['url'],
+                out=out,
+                **kwargs)
+    return out
 
 
 def get_teams(org_slug=DEFAULT_ORG_SLUG):

commit b9832d3517af1ea9eb51ac24d47680e79d279f82

Fix output type in github.get_paginated_content
The json response from the Github API (and by extension the `out`
variable in `get_paginated_content`) should be a `list` not a `dict`.

It looks like this typo was introduced in a refactor in commit cbca00e
where the out variable was wrongly assumed to be a `dict`. It was missed
by the tests because the `get_paginated_content` function was always
mocked. I added an additional test class to specfically test that
function.

Also, providing mutable values as function defaults causes unexpected
behaviour where the same mutable value is shared across all calls to the
function in that thread. I have added a test for this behaviour and
moved the default checking into the function.

bootstrap_salt/deploy_lib/github.py
@@ -49,7 +49,7 @@ def get_github_token():
 
 def get_paginated_content(url,
                           page=None,
-                          out={},
+                          out=None,
                           **kwargs):
     """
     Helper function to get information from the github API,
@@ -59,10 +59,14 @@ def get_paginated_content(url,
         url(string): The API url endpoint to contact
         page(int): DEPRECATED: The page number of the
             paginated requests to retrieve.
-        out(dict): Output dictionary to append results to.
+        out(list): Output list to append results to.
         kwargs(dictionary): Other API call parameters
     """
     per_page = 100
+
+    # Ensure out defaults to an empty list
+    if out is None:
+        out = []
 
     # Ensure we have a params entry in kwargs
     if 'params' not in kwargs:
@@ -105,7 +109,7 @@ def get_teams(org_slug=DEFAULT_ORG_SLUG):
         org_slug(string): The organisation name in slug format
 
     Returns:
-        response(dict): Dictionary of the response from github
+        response(list): List of teams from github
     """
     # Ensure that org name is in slug format
     forced_org_slug = check_slug_format(org_slug)
@@ -122,7 +126,7 @@ def get_org_members(org_slug=DEFAULT_ORG_SLUG):
         org_slug(string): The organisation name in slug format
 
     Returns:
-        response(dict): Dictionary of the response from github
+        response(list): List of organisation members from github
     """
     # Ensure that org name is in slug format
     forced_org_slug = check_slug_format(org_slug)
@@ -166,7 +170,7 @@ def get_team_members(team_slug, org_slug=DEFAULT_ORG_SLUG):
         org_slug(string): The organisation name in slug format
 
     Returns:
-        (dict): Dictionary of the response from github
+        (list): List of team members from github
     """
     # Ensure that team name is in slug format
     forced_team_slug = check_slug_format(team_slug)

tests/github_tests.py
@@ -188,3 +188,45 @@ class GithubTest(unittest.TestCase):
         # existing user with key
         self.assertEquals(github.get_user_keys('Slug User'),
                           [self.public_keys['rsa']])
+
+
+class GithubPaginatedContentTest(unittest.TestCase):
+
+    def setUp(self):
+        self.get_patcher = mock.patch('bootstrap_salt.deploy_lib.github.requests.get')
+        self.get = self.get_patcher.start()
+
+    def tearDown(self):
+        self.get_patcher.stop()
+
+    def _set_github_response(self, **kwargs):
+        self.get.return_value = self._make_result(**kwargs)
+
+    def _make_result(self, status_code=200, links=None, json=None):
+        result = mock.Mock(name='Response')
+        result.status_code = status_code
+        result.links = links or {}
+        result.json.return_value = json or []
+
+        return result
+
+    def test_get_paginated_content(self):
+        with mock.patch.dict('os.environ', {'GH_TOKEN': 'a-token'}):
+            self._set_github_response(json=[{'some': 'value'}])
+
+            result = github.get_paginated_content('a-url')
+
+            self.assertEqual(result, [{'some': 'value'}])
+            self.get.assert_called_once_with(
+                'a-url',
+                auth=('a-token', 'x-oauth-basic'),
+                params={'per_page': 100})
+
+    def test_get_paginated_content_multiple_times(self):
+        with mock.patch.dict('os.environ', {'GH_TOKEN': 'a-token'}):
+            self._set_github_response(json=[{'some': 'value'}])
+
+            github.get_paginated_content('a-url')
+            result = github.get_paginated_content('a-url')
+
+            self.assertEqual(result, [{'some': 'value'}])