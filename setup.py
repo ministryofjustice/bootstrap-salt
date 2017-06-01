c57b3ca264dbe87b3436d0f64f31dbf345145e47
Yufang Zhang <yufangzhang@users.noreply.github.com>
2017-06-01 15:17:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bootstrap_salt',
-    version='1.3.6',
+    version='1.4.0',
     url='http://github.com/ministryofjustice/bootstrap-salt/',
     license='LICENSE',
     author='MOJDS',
